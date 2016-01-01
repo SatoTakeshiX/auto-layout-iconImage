8692105e8207f9ab6ace494b036a3ae3c7d0f78a
satoutakeshi <sato.takeshi1987@gmail.com>
2016-01-01 10:11:36 +0900

    Initial Commit

---
+++ iconImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iconImage
+//
+//  Created by satoutakeshi on 2016/01/01.
+//  Copyright © 2016年 satoutakeshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
